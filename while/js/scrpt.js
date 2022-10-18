ba9a54b7af9c8ae47af73f0c1d4d78ac745b6f04
giorgip62 <giorgiop62@gmail.com>
2022-10-18 16:20:03 +0200

    lista con while

---
+++ while/js/scrpt.js
@@ -0,0 +1,9 @@
+// lista della spesa usando ciclo while
+
+const arrayAlimenti = ['zuccchero' ,'farina','uova','latte'];
+contatore = 0;
+
+while(contatore < arrayAlimenti.length){
+  console.log(arrayAlimenti[contatore]);
+  contatore ++;
+}
